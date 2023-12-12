9ff3a4a3919ea5de1f07562c1bfff39589d35fea
Chit Hindocha <chithindocha1393@gmail.com>
2023-12-12 14:42:46 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
+import numpy as np
 import pickle
 
 app = Flask(__name__)
 
 with open('RandomForest.pkl', 'rb') as file:
     model = pickle.load(file)
+
+def make_prediction(input_data):
+    
+    input_data = np.array(input_data).reshape(1, -1) 
+    
+    predicted_class = model.predict(input_data)
+    predicted_prob = model.predict_proba(input_data)
+    
+    if np.max(predicted_prob) >= 0.50:
+        return predicted_class[0]
+    else:
+        return None
 
 @app.route('/')
 def home():
@@ -20,8 +33,13 @@ def predict():
                     float(request.form['ph']),
                     float(request.form['rainfall']),
                     ]
-        prediction1 = model.predict([features])[0]
+        user_input = [float(request.form.get('nitrogen')), float(request.form.get('phosphourus')), float(request.form.get('potassium')),
+                      float(request.form.get('temperature')), float(request.form.get('humidity')), float(request.form.get('ph')),
+                      float(request.form.get('rainfall'))]
+        prediction1 = make_prediction(user_input)
         url = f"static/images/{prediction1}.jpg"
+        if prediction1== None:
+            prediction1 = 'Cant grow any crops'
 
         return render_template('result.html', prediction1=prediction1, url = url)
 
